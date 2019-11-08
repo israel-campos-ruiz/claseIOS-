80e3ea1bcf09c2a67687fe94db70ca33e72e9abd
Gerardo Ochoa <gerardo.buenaonda@gmail.com>
2019-11-08 17:12:40 -0600

    primer commit de prueba

---
+++ proyectoPruebaSwiftClase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyectoPruebaSwiftClase
+//
+//  Created by Laboratorio UNAM-Apple 02 on 11/8/19.
+//  Copyright © 2019 GerrysLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
